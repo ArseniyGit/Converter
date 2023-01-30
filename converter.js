70361b47c7750d2b6232276ad15920c7bf4b8452
ArseniyGit <123206469+ArseniyGit@users.noreply.github.com>
2023-01-30 17:24:56 +0300

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,11 @@
+const RATES = {
+    usd: 0.014,
+    eur: 0.013
+};
+
+    function convert({rub, currency}){
+        if (!RATES[currency]){
+            return null;
+        }
+        return rub * RATES[currency];
+    }
